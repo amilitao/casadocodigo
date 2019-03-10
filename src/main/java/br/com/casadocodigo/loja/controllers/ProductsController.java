8cdc8590d2b639a9d54c8d54b53f1e5e5dd899d2
adriano <adriano.militao@yahoo.com.br>
2019-03-09 22:47:28 -0300

    criando classe ProductDao

---
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
@@ -0,0 +1,29 @@
+package br.com.casadocodigo.loja.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.casadocodigo.loja.daos.ProductDAO;
+import br.com.casadocodigo.loja.models.Product;
+
+@Controller
+public class ProductsController {
+	
+	@Autowired
+	private ProductDAO productDAO;
+	
+	@RequestMapping("/produtos")
+	public String save(Product product) {
+		
+		System.out.println("Cadastrando o produto " + product);
+		return "products/ok";
+	}
+	
+	
+	@RequestMapping("/produtos/form")
+	public String form(){
+	return "products/form";
+	}
+
+}
